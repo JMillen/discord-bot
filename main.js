4ea38161edab11712177b57f9fb53b6b5aa75748
SHACKSS <66464271+SHACKSS@users.noreply.github.com>
2020-06-06 01:13:10 +1200

    update2

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+//variables
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
 //termianl ready message
 client.on('ready', function (){
     console.log(`${bot.user.username} Is online!`);
 });
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //if user says "hello" the bot will reply with =
 client.on('message', msg=>{
@@ -15,18 +16,35 @@ client.on('message', msg=>{
 });
 
 
-//if user says "hello" the bot will reply with =
+//if user says "beet is" the bot will reply with =
 client.on('message', msg=>{
     if(msg.content === "beet is"){
         msg.reply('my lord and saviour');
     }
 });
 
+//if user says "help" the bot will reply with =
+client.on('message', msg=>{
+    if(msg.content === "help"){
+        msg.reply('commands list: hello, beet is');
+    }
+});
+
+//if user says "" the bot will reply with =
+client.on('message', msg=>{
+    const attachment = new Attachment('https://tenor.com/view/beetlejuice-beet-beetlejuice-lester-green-lester-green-beetle-gif-12561400')
+    message.channel.send(message.author, attachment);
+    
+});
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//if user leaves message will be displayed
 client.on('guildMemberRemove', member => {
 
 });
 
-//member join message and auto role add
+//member join message 
 client.on("guildMemberAdd", (member) => {
     const channel = member.guild.channels.cache.find(channel => channel.name === "✝-the-hub-✝");
     if(!channel) return;
